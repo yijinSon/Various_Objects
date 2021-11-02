edcb5ee0cd191d240e5ef372a29ac83a898d078e
yijinSon <93176748+yijinSon@users.noreply.github.com>
2021-11-02 13:58:00 +0900

    add p5 codesource

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  fill(0);
+  rect(50,50,300,300);
+  fill(255);
+  rect(100,100,200,200);
+  fill(0);
+  rect(150,150,100,100);
+}
