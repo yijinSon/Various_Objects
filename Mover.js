ffb6860f50dc232046c7397abccfdf2caccbe494
yijinSon <93176748+yijinSon@users.noreply.github.com>
2021-11-02 15:03:29 +0900

    edit mouse attract

--- Mover.js
+++ Mover.js
@@ -4,20 +4,26 @@ class Mover {
     this.vel = createVector();
     this.acc = createVector();
     this.m = m;
-    
+
   }
-  
+
   applyForce(aForce) {
     let f = p5.Vector.div(aForce, this.m);
     this.acc.add(f);
   }
-  
+
+  attractTo(aPos){
+    let f = p5.Vector.sub(aPos, this.pos);
+    f.limit(2);
+    this.applyForce(f);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-  
+
   show() {
     fill(0);
     circle(this.pos.x, this.pos.y, this.m);
--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
-let m;
+let m, m2;
 
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
+  m2 = new Mover(350, 350, 50);
 }
 
 function draw() {
   background(220);
+
   m.update();
   m.show();
 
+  m2.update();
+  m2.show();
+
   if (mouseIsPressed) {
-    m.pos.x = mouseX;
-    m.pos.y = mouseY;
+    let mousePos = createVector (mouseX, mouseY);
+    m.attractTo(mousePos);
+    m2.attractTo(mousePos);
   }
 }
